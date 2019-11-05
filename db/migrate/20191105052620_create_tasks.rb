ff3f0b59c32fb72498d2869cdf837d7cbcde2742
sakiyamayurie <yuri01301208@gmail.com>
2019-11-05 06:23:17 +0000

    C2

---
+++ db/migrate/20191105052620_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :email
+      t.datetime :created_at
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190518043120) do
+ActiveRecord::Schema.define(version: 20191105052620) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.integer "user_id"
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
