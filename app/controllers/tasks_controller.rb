432767a0bba4ade8ba657e416398091bf188bb55
sakiyamayurie <yuri01301208@gmail.com>
2019-11-11 14:22:42 +0000

    C5途中8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,11 +12,17 @@ class TasksController < ApplicationController
   end
   
   def new
-    
+    @task = Task.new
   end
   
   def create
-    
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
   end
   
   private
