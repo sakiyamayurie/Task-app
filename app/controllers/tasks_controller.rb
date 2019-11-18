fa1ddac470809a10e25b8259f6bf1dc46967947a
sakiyamayurie <yuri01301208@gmail.com>
2019-11-18 14:08:45 +0000

    C5途中12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,30 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
   def index
-    @users = User.all
-    @tasks = @user.tasks
+    @user = User.find(params[user.id])
+    @task = @user.tasks
   end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    
+  end
+  
+  private
+  
+    def task_params
+      params.repuire(:task).permit(:name, :description)
+    end
+    
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'tasks/index'
 
   root 'static_pages#top'
   get '/signup', to: 'users#new'
