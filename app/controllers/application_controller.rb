85c43982f48f20672b43727af7ae3303168c6210
sakiyamayurie <yuri01301208@gmail.com>
2019-11-20 14:25:50 +0000

    C5途中14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end 
+  
+  # 現ログインユーザーであるか
+  def correct_user
+    redirect_to root_url unless current_user?(@user)
+  end
+  
 end
