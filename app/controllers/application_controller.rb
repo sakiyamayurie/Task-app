299a778e7bb4b9fcb506d2fb4f68b1020559f162
sakiyamayurie <yuri01301208@gmail.com>
2019-11-21 15:46:31 +0000

    C5途中13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,17 @@ class ApplicationController < ActionController::Base
   def correct_user
     redirect_to root_url unless current_user?(@user)
   end
+
+  # 現ログインユーザーが管理者であるか
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
   
+  # @userが定義されている上で使用する
+  def admin_or_correct
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "権限がありません。"
+      redirect_to root_url
+    end  
+  end
 end
