169b7aedc3df76aa257e8e0452fc4cda328d1b95
sakiyamayurie <yuri01301208@gmail.com>
2019-12-21 15:55:59 +0000

    F5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = "ログインしました。"
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
